65f7571ba619271e2e23b3151e28375d65a76e20
Marc Portier <marc.portier@gmail.com>
2024-02-20 16:45:20 +0100

    cleanup test
    fixes #8

--- tests/test_store.py
+++ tests/test_store.py
@@ -1,110 +1,81 @@
 #! /usr/bin/env python
 from util4tests import run_single_test
 import pytest
-import os
 from string import ascii_lowercase
-from pyrdfstore.store import RDFStoreAccess, RDFStore, URIRDFStore
+from typing import List, Tuple
 import math
 import random
 from rdflib import Graph, URIRef
-from pyrdfstore.common import QUERY_BUILDER
+from pyrdfstore.store import RDFStore, URIRDFStore, SPARQLResult
+from logging import getLogger
 
 
-@pytest.mark.usefixtures("rdf_store")
-def test_ingest(rdf_store):
-    pass
+log = getLogger("tests")
 
 
-@pytest.mark.usefixtures("prepopulated_rdf_store")
-def test_select_subjects(prepopulated_rdf_store):
-    assert (
-        prepopulated_rdf_store is not None
-    ), "can't perform test without target store"
-    sparql = "SELECT ?subject ?p WHERE { ?subject ?p ?o }"
-    tsa = RDFStoreAccess(prepopulated_rdf_store, QUERY_BUILDER)
-    subjects = tsa.select_subjects(sparql)
-    assert isinstance(subjects, list)
-    assert len(subjects) > 0
-    # Add more assertions as needed
+@pytest.mark.usefixtures("rdf_store", "example_graphs")
+def test_fixtures(rdf_store: RDFStore, example_graphs: List[Graph]):
+    assert rdf_store is not None, "fixture rdf-store should be available."
+    assert example_graphs is not None, "fixture example_graphs should be available"
+    assert (len(example_graphs) == 10)
 
 
 def test_graph_to_batches():
     graph = Graph()
     groupsize = 2
-    max_line = 4096 / groupsize
-    stuffing = "<> <> <> . \n"
+    max_line = math.floor(4096 / groupsize)  # we want chunk-up to result in equal sized groups by having each line this short
+    stuffing = "<> <> <> . \n"   # the overhead chars that will be added
     available_len = max_line - len(stuffing)
-    # Add a million triples to the graph
+    # Add a bunch of triples to the graph
     cnt = 0
     for i in range(10):
         for c in ascii_lowercase:
-            j = int(math.floor(random.randint(0, available_len) / 3))
-            element_lengths = [i, j, (int(available_len) - (int(i) + int(j)))]
+            j = math.floor(random.randint(0, available_len) / 3)
+            uri_lengths = [i, j, available_len - (i + j)]
             # Create the list
-            elements = [URIRef(c * int(element_lengths[k])) for k in range(3)]
-            graph.add((elements[0], elements[1], elements[2]))
+            triple = [URIRef(c * int(uri_lengths[k])) for k in range(3)]
+            graph.add((triple[0], triple[1], triple[2]))
             cnt += 1
 
-    print(f"{len(graph)=}")
+    log.debug(f"{len(graph)=}")
     assert (
         len(graph) == cnt
     ), "we should have not created duplicate or missing triples"
     batches = URIRDFStore._graph_to_batches(graph)
-    assert len(batches) > 0
+
     # total number of batches should be 260
+    assert len(batches) > 0
     assert (
         len(batches) == cnt / groupsize
-    ), f"the amount of batches should be count of all triples over groupsize"
+    ), "the amount of batches should be count of all triples over groupsize"
     found_sizes = {len(grp.split("\n")) for grp in batches}
     expected_sizes = {groupsize}
 
     first_batch = batches[0].split("\n")
-    print(f"{first_batch=}")
+    log.debug(f"{first_batch=}")
     # print(f"First batch split by newline: {batches[0].split('\n')}")
     assert (
         found_sizes == expected_sizes
     ), f"all batches should be of size {expected_sizes} not {found_sizes}"
 
 
-@pytest.mark.usefixtures("rdf_store")
-def test_insert(rdf_store):
+@pytest.mark.usefixtures("rdf_store", "example_graphs")
+def test_insert(rdf_store, example_graphs):
     assert rdf_store is not None, "can't perform test without target store"
 
-    # Create a test graph
-    graph = Graph()
-    graph.add(
-        (
-            URIRef("http://example.org/subject1"),
-            URIRef("http://example.org/predicate1"),
-            URIRef("http://example.org/object1"),
-        )
-    )
-    graph.add(
-        (
-            URIRef("http://example.org/subject2"),
-            URIRef("http://example.org/predicate2"),
-            URIRef("http://example.org/object2"),
-        )
-    )
-
     # Call the insert method
-    rdf_store.insert(graph)
+    num_triples = 2
+    for i in range(num_triples):
+        rdf_store.insert(example_graphs[i])
 
     # Verify that the triples are inserted correctly
     sparql = "SELECT ?subject ?predicate ?object WHERE { ?subject ?predicate ?object }"
-    tsa = RDFStoreAccess(rdf_store, QUERY_BUILDER)
-    results = tsa.select_subjects(sparql)
-    assert len(results) == 2
-    assert (
-        URIRef("http://example.org/subject1"),
-        URIRef("http://example.org/predicate1"),
-        URIRef("http://example.org/object1"),
-    ) in results
-    assert (
-        URIRef("http://example.org/subject2"),
-        URIRef("http://example.org/predicate2"),
-        URIRef("http://example.org/object2"),
-    ) in results
+    results: List[Tuple[str]] = [tuple(str(u) for u in r) for r in rdf_store.select(sparql)]
+
+    assert len(results) == num_triples
+    log.debug(f"{results=}")
+    for i in range(num_triples):
+        assert tuple(f"https://example.org/{part}#{i}" for part in ["subject", "predicate", "object"]) in results
 
 
 if __name__ == "__main__":
