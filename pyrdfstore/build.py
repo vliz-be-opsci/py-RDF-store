d619d6dc256a81006f1968408ea756775492712e
Marc Portier <marc.portier@gmail.com>
2024-02-21 18:39:44 +0100

    cleanup the factory method to make it more python-esk

--- pyrdfstore/build.py
+++ pyrdfstore/build.py
@@ -3,30 +3,20 @@ from pyrdfstore.store import (
     MemoryRDFStore,
     URIRDFStore,
 )
-from pyrdfstore.common import QUERY_BUILDER
-from typing import Optional, List
-from pyrdfj2 import J2RDFSyntaxBuilder
-import os
 import logging
 
 log = logging.getLogger(__name__)
 
 
-def create_rdf_store(rdf_store_info: Optional[List[str]] = None) -> RDFStore:
-    if rdf_store_info != None:
-        log.debug(len(rdf_store_info))
-        assert (
-            len(rdf_store_info) >= 2
-        ), "Invalid number of arguments. Max 2 arguments are allowed, {} were provided".format(
-            len(rdf_store_info)
-        )
+def create_rdf_store(*store_info) -> RDFStore:
+    """ Creates an rdf_store based on the passed non-None arguments.
+    0 of those arguments, will yield a MemoryRDFStore, 1-2 will be passed as read_uri resp write_uri to URIRDFStore
+    Anything beyond is unacceptable
+    """
+    store_info = [el for el in store_info if el is not None]  # remove possible None values
+    assert len(store_info) <= 2, "Too many arguments to create store {store_info=}"
 
-    if rdf_store_info == None:
+    if len(store_info) == 0:
         return MemoryRDFStore()
     # else
-    if len(rdf_store_info) == 1:
-        rdf_store_info.append(rdf_store_info[0])
-    return URIRDFStore(
-        read_uri=rdf_store_info[0],
-        write_uri=rdf_store_info[1],
-    )
+    return URIRDFStore(*store_info)
